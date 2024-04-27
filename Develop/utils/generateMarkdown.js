d59a163aac4fa55b9b3b0462980da623073d22c3
ddprzy37 <122937215+ddprzy37@users.noreply.github.com>
2024-04-27 11:58:23 -0600

    added license section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
@@ -11,7 +11,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+//  function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
@@ -24,11 +24,19 @@ function renderLicenseLink(license) {
           return '';
   }
 }
-// TODO: Create a function that returns the license section of README
+//  function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+      return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${license} license.`
+  } else {
+      return '';
+  }
+}
+
+//  function to generate markdown for README
 function generateMarkdown(data) {
   const licenseBadge = renderLicenseBadge(data.license);
   return `# ${data.title}
@@ -40,5 +48,6 @@ ${licenseBadge}
 module.exports = {
   renderLicenseBadge,
   generateMarkdown,
-  renderLicenseLink
+  renderLicenseLink,
+  renderLicenseSection
 };
